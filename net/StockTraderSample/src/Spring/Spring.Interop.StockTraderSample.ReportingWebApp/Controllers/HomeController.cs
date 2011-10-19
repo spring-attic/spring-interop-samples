8c7c5c6fec62a49c0baf7cdf75d961f6ff87965e
Steve Bohlen <sbohlen@gmail.com>
2011-10-19 15:16:49 -0400

    added adoTemplate to retrieve trade history

--- net/StockTraderSample/src/Spring/Spring.Interop.StockTraderSample.ReportingWebApp/Controllers/HomeController.cs
+++ net/StockTraderSample/src/Spring/Spring.Interop.StockTraderSample.ReportingWebApp/Controllers/HomeController.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Web.Mvc;
+using Spring.Data.Generic;
 using Spring.Interop.StockTraderSample.ReportingWebApp.Models;
 
 namespace Spring.Interop.StockTraderSample.ReportingWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private AdoTemplate _adoTemplate;
+        private IRowMapper<Trade> _tradeRowMapper;
 
         public string Message { get; set; }
+        public AdoTemplate AdoTemplate { set { _adoTemplate = value; } }
+        public IRowMapper<Trade> TradeRowMapper { set { _tradeRowMapper = value; } }
 
         public ActionResult Index()
         {
@@ -23,16 +30,25 @@ namespace Spring.Interop.StockTraderSample.ReportingWebApp.Controllers
 
         public ActionResult ActivityReport()
         {
-            var trades = new List<Trade>()
-                             {
-                                 new Trade("MSFT", 1, 1.0, Trade.OrderType.BUY),
-                                 new Trade("CSCO", 2, 1.5, Trade.OrderType.BUY),
-                                 new Trade("GOOG", 15, 35.45, Trade.OrderType.BUY),
-                                 new Trade("APPL", 12, 225.23, Trade.OrderType.BUY),
-                             };
-            TradeActivityModel tradeModel = new TradeActivityModel("Activity", trades);
+            var sql = "select Symbol, Quantity, ExecutionPrice, Type from Trade";
+
+            var trades = _adoTemplate.QueryWithRowMapper(CommandType.Text, sql, _tradeRowMapper);
+
+            var tradeModel = new TradeActivityModel("Activity", trades);
 
             return View(tradeModel);
+        }
+    }
+
+    public class TradeRowMapper : IRowMapper<Trade>
+    {
+        public Trade MapRow(IDataReader reader, int rowNum)
+        {
+            var typeString = reader.GetString(3);
+
+            var type = typeString == "BUY" ? Trade.OrderType.BUY : Trade.OrderType.SELL;
+
+            return new Trade(reader.GetString(0), reader.GetInt32(1), (double)reader.GetDecimal(2), type);
         }
     }
 }
